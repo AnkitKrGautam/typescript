3f5d26494c93a46616405521e175188235f5fc7f
AnkitKumarGautam <ankitkumarg-sdd>
2022-09-28 18:14:20 +0530

    function params

--- playground.ts
+++ playground.ts
@@ -1,3 +1,12 @@
-export default function play () {
-    console.log("Hello world!! play ")
+export default function play() {
+  var name = "Ankit";
+  var age = 25;
+
+  function logPersonInfo(personName: String, personAge: number) {
+    const info = `person name ${personName} and age is ${personAge}`;
+    console.log(info);
+    return info;
+  }
+  logPersonInfo(name, age);
+  logPersonInfo("ankit", 15);
 }
