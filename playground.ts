92a2166742aec76c81bed1b64cc857f92077589c
AnkitKumarGautam <ankitkumarg-sdd>
2022-09-29 12:58:57 +0530

    Interface vs type

--- playground.ts
+++ playground.ts
@@ -1,12 +1,23 @@
-type Person = {
-    name: string,
-    age: number
-}
-
-// interface Person {   //both custom types and interface are same only a little difference
+// type aliases
+// to describe function types
+// type Person = { //both custom types and interface are same only a little difference
 //     name: string,
 //     age: number
 // }
+
+// TS recommend to use Interface over Types when possible
+
+
+//Describe data stucture in more natural way 
+//Describe objects
+//Shipment, Orders
+
+type Data = string;
+
+interface Person {   
+    name: Data,
+    age: number
+}
 
 export default function play() {
   var name = "Ankit";
