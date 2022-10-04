36bb1396aa8e71c5f5a7ddcedc315fc794297f90
AnkitKumarGautam <ankitkumarg-sdd>
2022-10-04 17:27:34 +0530

    class types

--- playground.ts
+++ playground.ts
@@ -1,39 +1,51 @@
+// interface Person {
+//   name: string
+//   age: number
+// }
 
-interface Person {
-  name: string
-  age: number
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 }
-
 // interface PersonLoggerFn {
 //   (name: string, age: number): string
 // }
 
-type PersonLoggerFn = (name: string, age: number) => string
-
+type PersonLoggerFn = (name: string, age: number) => string;
 
 export default function play() {
-  const name: string = "Filip"
-  const age: number = 30
+  const name: string = "Filip";
+  const age: number = 30;
 
-  const person: Person = {
-    name: "John",
-    age: 34
-  }
+  // const person: Person = {
+  //   name: "John Doe",
+  //   age: 34,
+  // };
 
-
-  const logPersonInfo: PersonLoggerFn = (personName: string, personAge: number): string => {
-    const info = `Name: ${personName}, age: ${personAge}`
-    console.log(info)
-    return info
-  }
+  const logPersonInfo: PersonLoggerFn = (
+    personName: string,
+    personAge: number
+  ): string => {
+    const info = `Name: ${personName}, age: ${personAge}`;
+    console.log(info);
+    return info;
+  };
 
   function logPersonInfo2(person: Person) {
     // const info = "Name: " + personName + ", age: " + personAge
-    const info = `Name: ${person.name}, age: ${person.age}`
-    console.log(info)
-    return info
+    const info = `Name: ${person.name}, age: ${person.age}`;
+    console.log(info);
+    return info;
   }
 
-  const log = logPersonInfo(name, age)
-  logPersonInfo2(person)
+  const log = logPersonInfo(name, age);
+
+  const person = new Person("The Flash", 10000);
+
+  logPersonInfo2(person);
 }
